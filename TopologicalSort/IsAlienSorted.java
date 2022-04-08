0fce0a1dfe6e8a6cca309ac00907f13154425eda
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-04-08 07:50:56 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ TopologicalSort/IsAlienSorted.java
@@ -0,0 +1,28 @@
+class IsAlienSorted {
+    public boolean isAlienSorted(String[] words, String order) {
+
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < order.length(); i++) {
+            alphabet[order.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+
+                if (j >= words[i + 1].length()) return false;
+
+                if (words[i].charAt(j) != words[i + 1].charAt(j)) {
+
+                    int currentWordChar = words[i].charAt(j) - 'a';
+                    int nextWordChar = words[i + 1].charAt(j) - 'a';
+
+                    if (alphabet[currentWordChar] > alphabet[nextWordChar]) return false;
+                    else break;
+                }
+            }
+        }
+
+        return true;
+    }
+}
