53c2225564ea79ea12cb2b8e243a5d9cf42b48ce
Acceptance AI Inc <developers@acceptance.ai>
2022-04-14 01:18:58 -0500

    :smile: tim_bot :smile: via tim's deploy_script.sh

---
+++ Graph/DisjointSet/UnionByRank.java
@@ -0,0 +1,43 @@
+package Graph.DisjointSet;
+
+public class UnionByRank {
+
+    private int[] root;
+    private int[] rank;
+
+    public UnionByRank(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        while (x != root[x]) {
+            x = root[x];
+        }
+        return x;
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+}
---
+++ Graph/DisjointSet/UnionFind.java
@@ -0,0 +1,29 @@
+package Graph.DisjointSet;
+
+// UnionFind.class
+class UnionFind {
+    private int[] root;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
+                }
+            }
+        }
+    }
+}
