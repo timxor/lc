de655bd0d9f3596e0bf4bed9d76002af875e90c6
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-04-19 16:50:06 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ SystemDesign/InMemoryFileSystem.java
@@ -0,0 +1,51 @@
+//package SystemDesign;
+//
+//import java.util.*;
+//
+//// https://leetcode.com/problems/design-in-memory-file-system/
+//// hard
+//// amazon
+//
+//public class InMemoryFileSystem {
+//
+//    class File {
+//        boolean isFile = false;
+//        HashMap<String, File> files = new HashMap<>();
+//        String content = "";
+//    }
+//
+//    File root;
+//
+//    public InMemoryFileSystem() {
+//        root = new File();
+//    }
+//
+//    public List<String> ls(String path) {
+//
+//        File t = root;
+//        List<String> files = new ArrayList<>();
+//
+//    }
+//
+//    public void mkdir(String path) {
+//
+//    }
+//
+//    public void addContentToFile(String filePath, String content) {
+//
+//    }
+//
+//    public String readContentFromFile(String filePath) {
+//
+//    }
+//
+//    /**
+//     * Your FileSystem object will be instantiated and called as such:
+//     * FileSystem obj = new FileSystem();
+//     * List<String> param_1 = obj.ls(path);
+//     * obj.mkdir(path);
+//     * obj.addContentToFile(filePath,content);
+//     * String param_4 = obj.readContentFromFile(filePath);
+//     */
+//
+//}
