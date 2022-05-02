58db8cbe72dfc8de1b8f8839871dbe301bd50740
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-05-01 19:12:27 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- Math/ArmstrongNumber.java
+++ Math/ArmstrongNumber.java
@@ -21,20 +21,29 @@ public class ArmstrongNumber {
     public boolean isArmstrong(int n) {
 
         boolean isArmstrongNumber = false;
+
+        // convert int to String
         String nString = Integer.toString(n);
+
+        // get length of an int
         int length = String.valueOf(n).length();
         int sum = 0;
 
+        // convert int to int[] array
         int digits[] = new int[length];
         for(int i = 0; i < length; i++){
+
+            // convert int to int[] array
             digits[i] = nString.charAt(i) - '0';
         }
 
-        for (int i = 0; i < length; i ++){
+        for (int i = 0; i < length; i ++) {
             sum += Math.pow(digits[i], length);
         }
 
-        if (sum == n){isArmstrongNumber = true;}
+        if (sum == n) {
+            isArmstrongNumber = true;
+        }
 
         return isArmstrongNumber;
     }
