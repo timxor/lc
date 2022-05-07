1d52329c901aab6f1eb62d880ec3699c9d2fec77
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-05-06 20:32:51 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ Design/DetectSquares.java
@@ -0,0 +1,27 @@
+package Design;
+
+
+/*
+    google top 50
+    https://leetcode.com/problems/detect-squares/
+
+
+
+ */
+
+
+public class DetectSquares {
+
+    public DetectSquares() {
+
+    }
+
+    public void add(int[] point) {
+        return;
+    }
+
+    public int count(int[] point) {
+        return 0;
+    }
+
+}
