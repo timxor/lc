8678b1606c8edb34c30d07ed289af179377d6aaa
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-07-12 11:07:37 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ ArraysAndStrings/MeetingRooms.java
@@ -0,0 +1,17 @@
+package ArraysAndStrings;
+
+/*
+    https://leetcode.com/problems/meeting-rooms/
+
+
+
+ */
+
+
+public class MeetingRooms {
+
+    public boolean canAttendMeetings(int[][] intervals) {
+        return false;
+    }
+
+}
---
+++ ArraysAndStrings/MeetingRoomsII.java
@@ -0,0 +1,17 @@
+package ArraysAndStrings;
+
+/*
+
+
+
+    https://leetcode.com/problems/meeting-rooms-ii/
+
+
+ */
+public class MeetingRoomsII {
+
+    public int minMeetingRooms(int[][] intervals) {
+        return 0;
+    }
+
+}
