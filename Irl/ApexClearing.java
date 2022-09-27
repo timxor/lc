bded5d0214ed27d0c1884ac222784b56ae5ced38
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2022-09-27 13:45:49 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- Irl/ApexClearing.java
+++ Irl/ApexClearing.java
@@ -1,12 +1,16 @@
 package Irl;
 
 
+import java.util.Arrays;
+
 public class ApexClearing
 {
 
 
     public static void main(String[] args){
         init();
+
+        testMaxProfit();
     }
 
 
@@ -17,4 +21,77 @@ public class ApexClearing
         System.out.println("Stopping ApexClearning.java");
         System.out.println();
     }
+
+     /*
+            Best Time to Buy and Sell Stock
+
+            Approach 2: One Pass
+
+            Time Complexity: O(n)
+
+            Space Complexity: O(1)
+
+    */
+
+    private static void testMaxProfit() {
+
+        boolean testResult;
+
+        int[] inputPrices = new int[]{7,1,5,3,6,4};
+        int outputExpectedResult = 5;
+
+        int outputActualResult = maxProfit(inputPrices);
+        testResult = (outputExpectedResult == outputActualResult);
+
+        System.out.println("Test results for 'testMaxProfit():    "+ testResult );
+        System.out.println("    ");
+        System.out.println("    inputPrices = " + Arrays.toString(inputPrices) );
+        System.out.println("    outputExpectedResult = " + outputExpectedResult );
+        System.out.println("    outputActualResult = " + outputActualResult );
+        System.out.println("    ");
+    }
+
+
+
+    public static int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
 }
+
+
+
+/*
+
+NBBO = National Best Bid and Offer
+
+when buying
+best available (lowest) offer price
+
+when selling
+best available (highest) bid price
+
+
+sell(){
+    double nbboSellOrder = findBestHighestBidPrice()
+}
+
+
+
+buy(){
+    double nbboBuyOrder = findBestLowestOfferPrice()
+}
+
+
+
+
+
+
+ */
