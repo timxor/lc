f292857b9bd7f662d55d7dfd3a15d72410b3ff81
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-01-02 10:47:06 -0600

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ Irl/PowersOf10and2.java
@@ -0,0 +1,53 @@
+package Irl;
+
+public class PowersOf10and2 
+{
+  // powers of 10
+  
+      // 10^1
+      
+      // 10^2
+      
+      // 10^3 = 1,000
+      
+      // 10^4 = 10,000
+      
+      // 10^5 = 100,000 (one hundred thousand)
+      
+      // 10^6 = 1,000,000 (one million)
+      
+      // 10^7 = 10,000,000 (10 million)
+      
+      // 10^8 = 100,000,000 (100 million)
+      
+      // 10^9 = 1,000,000,000 (1 billion)
+      
+
+
+  // powers of 2
+  
+  
+    // 2^31 - 1
+  
+
+
+
+
+
+  // math rand
+  
+  
+    // get random
+    Random rand = new Random();
+    List<Integer> list = new ArrayList();
+    
+    int randomElement = list.get(rand.nextInt(list.size()));
+    array[rand.nextInt(array.size)]
+
+
+
+
+
+
+  
+}
