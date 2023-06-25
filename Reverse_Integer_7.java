7136242f6a40dd097641d09dc560cb01b3114443
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-06-25 11:33:21 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ Reverse_Integer_7.java
@@ -0,0 +1,51 @@
+//
+// Reverse_Integer_7 [here](https://leetcode.com/problems/reverse-integer/description/)
+//
+//
+
+class Reverse_Integer_7 {
+    public int reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
+
+/*
+
+This Java method is designed to reverse an integer number, returning the reversed value. Here is a detailed step-by-step explanation of how it works:
+
+1. The method takes an integer `x` as an argument.
+
+2. It initializes a variable `result` to 0, which will be used to store the reversed number.
+
+3. It enters a loop that continues as long as `x` is not equal to zero. This loop operates by continuously stripping off the last digit of `x` and appending it to `result`. When `x` becomes zero, it implies all the digits have been stripped off and appended to `result`.
+
+4. Within the loop:
+
+    - The last digit of `x` is determined with the line `int tail = x % 10;`. The `%` operator gives the remainder of division, so it effectively extracts the last digit.
+
+    - The reversed number so far is calculated by the line `int newResult = result * 10 + tail;`. Multiplying `result` by 10 shifts all its digits one place to the left, and the last digit `tail` is appended on the right.
+
+    - The next lines act as a check for integer overflow. If the `newResult` cannot be reverted back to the original `result` by subtracting `tail` and dividing by 10, it indicates an overflow situation. In case of an overflow, the function returns 0 immediately.
+
+    - If there is no overflow, `newResult` is then assigned back to `result`.
+
+    - The last digit is stripped off `x` with the line `x = x / 10;`. Division by 10 effectively removes the last digit.
+
+5. Once the loop finishes, the function returns the `result`, which is the original integer `x` in reversed form.
+
+Note: This method handles overflow by returning 0 in those cases. However, it does not handle negative numbers appropriately because it strips the minus sign. Also, trailing zeros in the original number will be omitted in the reversed number. For example, reversing `100` results in `1`, not `001` or `1`.
+
+*/
