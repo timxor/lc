dc9079faae0aba9ab1b0232ca0d555460959296d
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-09-28 14:25:03 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ Review.java
@@ -0,0 +1,23 @@
+// javac --release 21 --enable-preview Review.java
+// java --enable-preview Review
+
+/*
+    sdk list java
+    sdk use java 21-amzn
+    sdk default java 21-amzn
+    java --version
+    
+ */
+
+public class Review {
+    
+    void main() {
+        
+        System.out.println("");
+        System.out.println("Review.java");
+        System.out.println("");
+        System.out.println("");
+        
+        
+    }
+}
