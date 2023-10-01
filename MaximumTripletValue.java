939e691bf737a024dac84206abcf72ddd4a31bf4
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-10-01 02:27:20 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- MaximumTripletValue.java
+++ MaximumTripletValue.java
@@ -20,6 +20,25 @@ public class MaximumTripletValue {
         }
         return result;
     }
+
+
+
+//    public long maximumTripletValue(int[] nums) {
+//        int n = nums.length;
+//        long ans = 0;
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = i+1; j < n; ++j) {
+//                for (int k = j+1; k < n; ++k) {
+//                    long test = nums[k];
+//                    ans = Math.max(ans,test*(nums[i]-nums[j]));
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+    
+    
+    
 }
 
 
--- MaximumTripletValue2.java
+++ MaximumTripletValue2.java
@@ -1,2 +1,24 @@
 public class MaximumTripletValue2 {
+
+    public long maximumTripletValue(int[] nums) {
+
+        int max0 = nums[0];
+        long ans = 0L;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (max >= 0) {
+                ans = Math.max(ans, (long)max * (long)nums[i]);
+            }
+
+            max = Math.max(max, max0 - nums[i]);
+
+            max0 = Math.max(max0, nums[i]);
+
+        }
+        return ans;
+
+    }
+    
 }
