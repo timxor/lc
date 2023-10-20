da45739fe226ef124c74c1d168a67eea165904ce
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-10-20 18:47:05 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- Multiset.java
+++ Multiset.java
@@ -1,18 +1,56 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Multiset<T> {
-    private final Map<T, Integer> map = new HashMap<>();
+    private Map<T, Integer> map;
+
+    public Multiset() {
+        map = new HashMap<>();
+    }
 
     public void add(T element) {
         map.put(element, map.getOrDefault(element, 0) + 1);
     }
 
     public void remove(T element) {
-        map.put(element, Math.max(0, map.getOrDefault(element, 0) - 1));
+        if (map.containsKey(element)) {
+            int count = map.get(element);
+            if (count == 1) {
+                map.remove(element);
+            } else {
+                map.put(element, count - 1);
+            }
+        }
     }
 
     public int count(T element) {
         return map.getOrDefault(element, 0);
     }
+
+    public Set<T> elementSet() {
+        return map.keySet();
+    }
+
+    @Override
+    public String toString() {
+        return map.toString();
+    }
+
+    public static void main(String[] args) {
+        Multiset<String> multiset = new Multiset<>();
+        multiset.add("apple");
+        multiset.add("banana");
+        multiset.add("apple");
+
+        System.out.println("Multiset: " + multiset);
+        System.out.println("Count of 'apple': " + multiset.count("apple"));
+
+        multiset.remove("apple");
+        System.out.println("Multiset after removing one 'apple': " + multiset);
+
+        multiset.remove("apple");
+        System.out.println("Multiset after removing another 'apple': " + multiset);
+    }
 }
+
