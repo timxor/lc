3acaa862b59e2005bb575a4cd33dce9bc2724402
Tim Siwula <siwulactim@gmail.com>
2024-05-23 21:05:13 -0500

    :walking: [auto generated] via tim's deploy_script.sh

---
+++ TheNumberOfBeautifulSubsets.java
@@ -0,0 +1,23 @@
+
+// The Number of Beautiful Subsets
+//
+// #2597
+//
+// https://leetcode.com/problems/the-number-of-beautiful-subsets/
+
+
+
+class TheNumberOfBeautifulSubsets {
+    public int beautifulSubsets(int[] nums, int k) {
+
+        int result = 1;
+
+        for(int i = 0; i < nums.length; i++) {
+
+        }
+
+
+
+        return result;
+    }
+}
